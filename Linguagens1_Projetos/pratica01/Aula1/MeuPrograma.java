817b8cf45957a3a2052557fbafb649e3d245032b
GABRIEL  DE GODOY BRAZ <17.00163-3@maua.br>
2020-03-02 11:29:04 -0300

    Resposta das perguntas

---
+++ Linguagens1_Projetos/pratica01/Aula1/MeuPrograma.java
@@ -0,0 +1,9 @@
+class MeuPrograma {
+    public static void main (String[] args){
+        System.out.println("Minha primeira aplicação Java!");
+        System.out.println("Olá Mundo");
+        System.out.println("Estou programando no modo Conan!");
+    }
+
+
+}
