e0e4781e05f4607668a3d4b5636302d793ff7c3c
Gabriel Godoy <gabriel.godoybz@hotmail.com>
2020-03-03 23:27:02 -0300

    Resolução exercicio 2

--- Linguagens1_Projetos/pratica01/Exercícios/EXERC2/src/com/company/Main.java
+++ Linguagens1_Projetos/pratica01/Exercícios/EXERC2/src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String nome1, nome2;
+        int idade1, idade2;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro nome: ");
+        nome1 = scanner.next();
+        System.out.println("Digite a primeira idade: ");
+        idade1 = scanner.nextInt();
+        System.out.println("Digite o segundo nome: ");
+        nome2 = scanner.next();
+        System.out.println("Digite a segunda idade: ");
+        idade2 = scanner.nextInt();
+        if(idade1 > idade2) {
+            System.out.println("A diferença de idade entre " + nome1 + " e " + nome2 + " é de " + (idade1 - idade2) + " anos");
+        }else{
+            System.out.println("A diferença de idade entre " + nome1 + " e " + nome2 + " é de " + (idade2 - idade1) + " anos");
+        }
     }
 }
