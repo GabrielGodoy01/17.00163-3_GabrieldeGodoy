70b323789c07232612e7309d8a805d9276a8ae3c
GabrielGodoy01 <61150821+GabrielGodoy01@users.noreply.github.com>
2020-04-04 18:52:20 -0300

    Introdução aula 5
    Usando o static minha variavel contaObjetos (que pertence a classe MinhaClasse) todos os objetos tem acesso aquela variavel com static

---
+++ Linguagens1_Projetos/pratica05/aula04/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    MinhaClasse m1 = new MinhaClasse();
+	    MinhaClasse m2 = new MinhaClasse();
+	    MinhaClasse m3 = new MinhaClasse();
+        System.out.println("Total objetos: " + m3.getContaObjetos());
+    }
+}
---
+++ Linguagens1_Projetos/pratica05/aula04/src/com/company/MinhaClasse.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class MinhaClasse {
+    private static int contaObjetos = 0;
+
+    public MinhaClasse(){
+        contaObjetos = contaObjetos + 1;
+    }
+
+    public int getContaObjetos(){
+        return contaObjetos;
+    }
+}
