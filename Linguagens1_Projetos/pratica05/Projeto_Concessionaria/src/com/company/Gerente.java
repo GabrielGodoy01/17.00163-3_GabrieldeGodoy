d56954139ee36e281acf472bafdd28f9adc66cd4
GabrielGodoy01 <61150821+GabrielGodoy01@users.noreply.github.com>
2020-04-05 18:03:30 -0300

    Exercício Concessionaria
    Exercicio 1 e 2 da aula 5

---
+++ Linguagens1_Projetos/pratica05/Projeto_Concessionaria/src/com/company/Gerente.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Gerente {
+    private double salarioBase;
+    private final double comissao = 0.1;
+
+    //Construtor
+    public Gerente(double salarioBase){
+        this.salarioBase = salarioBase;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public double getSalarioFinal(double totalVendasMes){
+        return salarioBase + comissao * totalVendasMes + 0.5*salarioBase;
+    }
+
+    public boolean darAumento(Vendedor vendedor, double aumento){
+            double novoSalarioBase = vendedor.getSalarioBase() * aumento + vendedor.getSalarioBase();
+            return vendedor.setSalarioBase(novoSalarioBase, this);
+    }
+}
