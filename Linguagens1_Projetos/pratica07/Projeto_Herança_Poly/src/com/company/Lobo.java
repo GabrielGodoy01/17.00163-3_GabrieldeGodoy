dead42aa7158c54653c0ad92860e0e09ec32c8de
GabrielGodoy01 <61150821+GabrielGodoy01@users.noreply.github.com>
2020-04-27 18:41:43 -0300

    Parte da pratica07

---
+++ Linguagens1_Projetos/pratica07/Projeto_Herança_Poly/src/com/company/Lobo.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Lobo extends Animal {
+    @Override
+    public void comer() {
+        System.out.println("Chapeuzinho!");
+    }
+
+    public void cheirar(){
+        System.out.println("Vishhh");
+    }
+
+}
