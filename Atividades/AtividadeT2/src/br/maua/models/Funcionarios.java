3699eebedcc24c20fd9eceef6af9500c37f997f7
GabrielGodoy01 <61150821+GabrielGodoy01@users.noreply.github.com>
2020-06-08 10:34:50 -0300

    Interface PostarMensagem

--- Atividades/AtividadeT2/src/br/maua/models/Funcionarios.java
+++ Atividades/AtividadeT2/src/br/maua/models/Funcionarios.java
@@ -1,4 +1,38 @@
 package br.maua.models;
 
+import br.maua.enumeracoes.HorarioSistema;
+import br.maua.enumeracoes.TiposMembros;
+
 public class Funcionarios {
+    private String usuario;
+    private String email;
+    private TiposMembros tiposMembros;
+    private HorarioSistema horarioSistema;
+
+    public Funcionarios(String usuario, String email, TiposMembros tiposMembros) {
+        this.usuario = usuario;
+        this.email = email;
+        this.tiposMembros = tiposMembros;
+        this.horarioSistema = HorarioSistema.NORMAL;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public TiposMembros getTiposMembros() {
+        return tiposMembros;
+    }
+
+    public void setHorarioSistema(HorarioSistema horarioSistema) {
+        this.horarioSistema = horarioSistema;
+    }
+
+    public HorarioSistema getHorarioSistema() {
+        return horarioSistema;
+    }
 }
--- Atividades/AtividadeT2/src/br/maua/models/TesteDrive.java
+++ Atividades/AtividadeT2/src/br/maua/models/TesteDrive.java
@@ -1,11 +1,17 @@
 package br.maua.models;
 
 import br.maua.enumeracoes.HorarioSistema;
+import br.maua.enumeracoes.TiposMembros;
 
 public class TesteDrive {
     public static void run(){
+        Funcionarios func1 = new Funcionarios("a","b", TiposMembros.MOBILEMEMBERS);
+        Funcionarios func2 = new Funcionarios("b","b", TiposMembros.HEAVYLIFTERS);
+        Funcionarios func3 = new Funcionarios("c","b", TiposMembros.SCRIPTGUYS);
+        Funcionarios func4 = new Funcionarios("d","b", TiposMembros.BIGBROTHERS);
+
         System.out.println("Bem-vindo!");
-        System.out.println("Horário de Trabalho: " + HorarioSistema.EXTRA);
+        System.out.println("Horário de Trabalho: " + func1.getHorarioSistema());
 
     }
 }
