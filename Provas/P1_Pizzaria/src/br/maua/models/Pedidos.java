4e1e3809f5efd7916020a69f0f7919bc6ae7897e
GabrielGodoy01 <61150821+GabrielGodoy01@users.noreply.github.com>
2020-06-12 00:20:42 -0300

    Update Pedidos.java

--- Provas/P1_Pizzaria/src/br/maua/models/Pedidos.java
+++ Provas/P1_Pizzaria/src/br/maua/models/Pedidos.java
@@ -3,6 +3,8 @@ package br.maua.models;
 import br.maua.enums.Estado;
 import br.maua.enums.FormaPagamento;
 
+import java.util.Random;
+
 public class Pedidos {
     private String id;
     private String descricao;
@@ -10,11 +12,19 @@ public class Pedidos {
     private FormaPagamento formaPagamento;
     private Estado estado;
 
-    public Pedidos(String id, String descricao, double valor, FormaPagamento formaPagamento) {
-        this.id = id;
+    public Pedidos(String descricao, double valor, FormaPagamento formaPagamento) {
+        this.id = geradorId();
         this.descricao = descricao;
         this.valor = valor;
         this.formaPagamento = formaPagamento;
         this.estado = Estado.REALIZADO;
+    }
+
+    private String geradorId() {
+        Random random = new Random();
+        String idGerado = "";
+        for (int i=0; i < 3; i++)
+            idGerado += random.nextInt(10);
+        return idGerado;
     }
 }
