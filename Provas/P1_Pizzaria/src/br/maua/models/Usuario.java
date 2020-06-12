5554f3adf1233cfe0ceedac1503d10ad49e908f5
GabrielGodoy01 <61150821+GabrielGodoy01@users.noreply.github.com>
2020-06-12 00:03:10 -0300

    Criação da classe Pedidos

--- Provas/P1_Pizzaria/src/br/maua/models/Usuario.java
+++ Provas/P1_Pizzaria/src/br/maua/models/Usuario.java
@@ -1,4 +1,18 @@
 package br.maua.models;
 
 public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = "123456";
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
 }
