6661c8d965ab160386391ad0c614911c8060f8bf
GabrielGodoy01 <61150821+GabrielGodoy01@users.noreply.github.com>
2020-08-23 19:28:47 -0300

    JavaFX - Parte 2

--- Projetos-JavaFX/Proj2/src/br/maua/ControleInterface.java
+++ Projetos-JavaFX/Proj2/src/br/maua/ControleInterface.java
@@ -1,9 +1,13 @@
 package br.maua;
 
+import br.maua.model.Usuario;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 public class ControleInterface {
@@ -15,8 +19,58 @@ public class ControleInterface {
     private TextField txtEntrada;
 
     @FXML
+    private TextField txtNumero1;
+    @FXML
+    private TextField txtNumero2;
+    @FXML
+    private Button btnSomar;
+
+    @FXML
+    private TextField txtNome, txtSenha, txtUrl;
+    @FXML
+    private ImageView imgFoto;
+
+    @FXML
     public void exibirNome(){
         String nome = txtEntrada.getText();
         lblSaida.setText(nome);
     }
+
+    @FXML
+    public void somar(){
+        int numero1 = Integer.parseInt(txtNumero1.getText());
+        int numero2 = Integer.parseInt(txtNumero2.getText());
+        lblSaida.setText("Saída: " + (numero1 + numero2));
+    }
+
+    @FXML
+    public void cadastrar(){
+        Usuario usuario = new Usuario(txtNome.getText(), txtSenha.getText(), txtUrl.getText());
+        txtNome.clear();
+        txtSenha.clear();
+        txtUrl.clear();
+        System.out.println("Usuário Cadastrado: " + usuario);
+    }
+
+    @FXML
+    public void previewPhoto(){
+        try {
+            String url = txtUrl.getText();
+            Image image = new Image(url);
+            imgFoto.setImage(image);
+        }
+        catch (IllegalArgumentException exception){
+            txtUrl.clear();
+            txtUrl.requestFocus();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("URl Incorreta");
+            alert.setHeaderText("Batata!");
+            // Para aa execução do restanet do código do método até ele ser concluído (Alerta)
+            alert.showAndWait();
+            //Deixa o restante do código ser executado
+            alert.show();
+            System.out.println("BATATA!");
+        }
+    }
+
 }
---
+++ Projetos-JavaFX/proj3_pokedex/src/br/alura/controller/PokedexController.java
@@ -0,0 +1,4 @@
+package br.alura.controller;
+
+public class PokedexController {
+}
