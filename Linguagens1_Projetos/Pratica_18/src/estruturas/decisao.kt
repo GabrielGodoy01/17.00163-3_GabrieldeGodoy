638328316042ac288738ed878ec274f1fe4a630a
GabrielGodoy01 <61150821+GabrielGodoy01@users.noreply.github.com>
2020-10-26 11:38:34 -0300

    Começo Pratica18

---
+++ Linguagens1_Projetos/Pratica_18/src/estruturas/decisao.kt
@@ -0,0 +1,14 @@
+package estruturas
+
+fun main() {
+    val valor = 42
+    if(valor > 10){
+        print("Maior que 10!")
+    }
+
+    if(valor < 10) {
+        println("valor menor q 10")
+    }else {
+        print("dale")
+    }
+}
---
+++ Linguagens1_Projetos/Pratica_18/src/fundamentos/variaveis_saida.kt
@@ -0,0 +1,11 @@
+package fundamentos
+
+fun main() {
+    //Declara uma variavel
+    var meuValor = 34.5
+    meuValor = 3.0
+    var meuNome : String
+    meuNome = "Ashura Yamashira"
+    println("Meu nome é $meuNome meu valor é $meuValor")
+
+}
