a4fb3c06c39586531427f3a1c92bf54cc4389a59
GabrielGodoy01 <gabriel.godoybz@hotmail.com>
2021-04-08 14:31:16 -0300

    Terceira aula finalizada

--- ArquiteturaSO_Workspace/gerenciamento_de_lembretes/lembretes/index.js
+++ ArquiteturaSO_Workspace/gerenciamento_de_lembretes/lembretes/index.js
@@ -1,8 +1,37 @@
 const express = require ('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+
+const lembretes = {   
+    // objeto json
+};
+contador = 0;
 
 app.get('/lembretes', (req, res) => {
-    res.status(200).send({"resultado": "OK"});
+    //res.status(200).send({"resultado": "OK"});
+    res.send(lembretes);
+});
+
+app.put('/lembretes', (req, res) => {
+    //objeto json{texto: "Fazer café"}
+    const {texto} = req.body;
+    contador++;
+    /*
+        {
+            1: {
+                contador: 1, texto: fazer cafe
+            }
+            2:{
+                contador: 2, texto: ver um filme
+            }
+        }
+    */
+    lembretes[contador] = {
+        contador, texto
+    }
+    res.status(201).send(lembretes[contador]);
 });
 
 app.listen(4000, () => {
